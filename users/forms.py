d1956dfbd3aeb2f20b6754d15bae24a810c196b2
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2021-10-05 22:33:43 +0100

    added validation to firstname,lastname, and password

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, ValidationError, Length, EqualTo
+
+
+def character_check(form,field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
-    firstname = StringField(validators=[Required()])
-    lastname = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required(), character_check])
+    lastname = StringField(validators=[Required(), character_check])
     phone = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required()])
-    confirm_password = PasswordField(validators=[Required()])
-    pin_key = StringField(validators=[Required()])
-    submit = SubmitField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12, message=
+    'Password must be between 6 and 12 characters in length')])
+    confirm_password = PasswordField(validators=[Required(), EqualTo('password', message=
+    'Both password fields must be equal!')])
+    pin_key = StringField(validators=[Required(), Length(min=32, max=32, message=
+    'Pin Key must be 32 characters in length')])
+    submit = SubmitField(validators=[Required(), ])
