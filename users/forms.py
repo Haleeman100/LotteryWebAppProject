720ffb23495e44a2bd0ed443b7ae0f915479e2b1
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2021-10-05 23:52:18 +0100

    added comments in forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,8 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, ValidationError, Length, EqualTo
 
 
-def character_check(form,field):
+# function to not allow user to make use of special characters in password
+def character_check(form, field):
     excluded_chars = "*?!'^+%&/()=}][{$#@<>"
     for char in field.data:
         if char in excluded_chars:
@@ -13,18 +14,32 @@ def character_check(form,field):
 
 
 class RegisterForm(FlaskForm):
+    # check for valid email address
     email = StringField(validators=[Required(), Email()])
+
+    # check if field is filled and if invalid characters are used
     firstname = StringField(validators=[Required(), character_check])
+
+    # check if field is filled and if invalid characters are used
     lastname = StringField(validators=[Required(), character_check])
+
+    # check if field is filled and if correct format is used
     phone = StringField(validators=[Required()])
+
+    # check if field is filled and requirements are satisfied
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message=
     'Password must be between 6 and 12 characters in length')])
+
+    # check if field is filled and requirements are satisfied
     confirm_password = PasswordField(validators=[Required(), EqualTo('password', message=
     'Both password fields must be equal!')])
+
+    # check if field is filled and requirement is satisfied
     pin_key = StringField(validators=[Required(), Length(min=32, max=32, message=
     'Pin Key must be 32 characters in length')])
     submit = SubmitField(validators=[Required(), ])
 
+    # function to allow user to make use of special characters in password
     def validate_password(self, password):
         p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[?@!#%^&*()<>$/{}~])')
         if not p.match(self.password.data):
