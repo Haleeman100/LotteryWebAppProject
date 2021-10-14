eb63e57b791d936e9c4e6f2957217037d76c37f6
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2021-10-14 10:54:33 +0100

    Deleted non required function for update in models

--- lottery/views.py
+++ lottery/views.py
@@ -2,13 +2,15 @@
 import logging
 
 from flask import Blueprint, render_template, request, flash
-
+from models import User, Draw
 from app import db
-from models import Draw
+
 
 # CONFIG
 lottery_blueprint = Blueprint('lottery', __name__, template_folder='templates')
 
+user = User.query.first()
+draw_key = user.draw_key
 
 # VIEWS
 # view lottery page
@@ -25,7 +27,7 @@ def add_draw():
     submitted_draw.strip()
 
     # create a new draw with the form data.
-    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0)  # TODO: update user_id [user_id=1 is a placeholder]
+    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0, draw_key=draw_key)  # TODO: update user_id [user_id=1 is a placeholder]
 
     # add the new draw to the database
     db.session.add(new_draw)
@@ -44,6 +46,7 @@ def view_draws():
 
     # if playable draws exist
     if len(playable_draws) != 0:
+
         # re-render lottery page with playable draws
         return render_template('lottery.html', playable_draws=playable_draws)
     else:
